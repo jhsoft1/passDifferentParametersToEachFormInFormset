84db5323f3679d438c1749fb7804ab7642c992cd
jhsoft1 <jens@petja.de>
2022-03-30 18:22:44 +0200

    source

--- questions/models.py
+++ questions/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 
+# https://forum.djangoproject.com/t/pass-different-parameters-to-each-form-in-formset/4040/2
+
 class Question(models.Model):
     text = models.CharField(max_length=100)
