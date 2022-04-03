29572896f7d513f65bcbc859a00e72370d0f7277
jhsoft1 <jens@petja.de>
2022-04-03 13:43:51 +0200

    answer table

--- questions/models.py
+++ questions/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.db.models import DO_NOTHING
 
 
 # https://forum.djangoproject.com/t/pass-different-parameters-to-each-form-in-formset/4040/2
 
+
 class Question(models.Model):
     text = models.CharField(max_length=100)
+
+    def __str__(self):
+        print(f'{self.text}')
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=100)
+    question = models.ForeignKey(Question, on_delete=DO_NOTHING)
+
+    def __str__(self):
+        print(f'{self.text} {self.question}')
